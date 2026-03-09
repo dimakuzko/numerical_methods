c9d8049a684426d984464c2567960a0454725f14
Pozhyla <mrbaymiks@gmail.com>
2026-03-09 14:23:42 +0100

    Delete trash

--- Class_01/Problem_02.m
+++
@@ -1,4 +0,0 @@
-x = linspace(0, pi, 10);
-y = cos(x);
-
-plot(x, y);
--- Class_03/Problem_03.m
+++
@@ -1,7 +0,0 @@
-clear all; clc;
-
-g = @(x) (2*x.^3 - 7*x.^2 + 6) / (3*x.^2 - 14*x + 14);
- 
-p0 = 1;
-tolerance = 1e-14;
-[answer, n] = fixed_point(g, p0, tolerance)
