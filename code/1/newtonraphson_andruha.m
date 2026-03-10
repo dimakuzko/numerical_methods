11e758e5dd2f902f6a12ee67f19c202af4bfdbec
Pozhyla <mrbaymiks@gmail.com>
2026-03-10 21:20:38 +0100

    Cleansing

--- code/1/newtonraphson_andruha.m
+++ code/1/newtonraphson_andruha.m
@@ -1,32 +1,26 @@
-function [p, n] = newtonraphson(f, df, p0, tolerance)
+function [p, n] = newtonraphson_andruha(f, df, p0, tolerance)
     p_old = p0;
     errors = [];
 
     for n = 1:1000
         df_p_old = df(p_old);
         if df_p_old == 0
-            error('Derivative is zero. No solution found.');
+            break
         end
         f_p_old = f(p_old);
         p_current = p_old - f_p_old / df_p_old;
 
         errors(n) = abs(p_current - p_old);
-        if n > 1
-            linear_ratio = errors(n) / errors(n-1);
-            quadratic_ratio = errors(n) / (errors(n-1)^2);
-            fprintf('Iter %d: Linear Ratio: %.4f | Quadratic Ratio: %.4f\n', n, linear_ratio, quadratic_ratio);
-        end
 
         if n > 2
             estimated_alpha = log(errors(n)/errors(n-1)) / log(errors(n-1)/errors(n-2));
-            fprintf('Step %d: Estimated Alpha = %.4f\n', n, estimated_alpha);
+            fprintf('Step: %d Estimated Alpha = %f\n', n, estimated_alpha);
         end
 
         if abs(p_current - p_old) < tolerance
             p = p_current;
-            return;
+            return
         end
         p_old = p_current;
     end
-    p = "Not Defined";
 end
--- code/1/script.m
+++ code/1/script.m
@@ -9,14 +9,17 @@ tol = 10^-15;
 
 [bis_p, bis_n] = bisection(f, p0, p1, tol);
 
+[new1_p, new1_n] = newtonraphson_andruha(f, df, p0, tol);
+
+[new2_p, new2_n] = newtonraphson_andruha(f, df, p1, tol);
+error_newtonraphson_p1_with_newtonraphson_p0 = abs(new1_p - new2_p)
+
 [sec_p, sec_n] = secant(f, p0, p1, tol);
+error_secant_with_newtonraphson = abs(new1_p - sec_p)
 
-[new1_p, new1_n] = newtonraphson(f, df, p0, tol);
-
-[new2_p, new2_n] = newtonraphson(f, df, p1, tol);
 
 % Display the results of the root-finding methods
 disp(['Bisection method: p = ', num2str(bis_p), ', iterations = ', num2str(bis_n)]);
-disp(['Secant method: p = ', num2str(sec_p), ', iterations = ', num2str(sec_n)]);
 disp(['Newton-Raphson method (0): p = ', num2str(new1_p), ', iterations = ', num2str(new1_n)]);
 disp(['Newton-Raphson method (pi/4): p = ', num2str(new2_p), ', iterations = ', num2str(new2_n)]);
+disp(['Secant method: p = ', num2str(sec_p), ', iterations = ', num2str(sec_n)]);
